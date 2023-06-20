bdb7bed3738f1cc1c17f558e372e05de948bd9a4
Gabriel Majeri <gabriel.majeri6@gmail.com>
2023-06-20 20:51:26 +0300

    Upload the scripts

---
+++ cat-dirs.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+
+import argparse
+from pathlib import Path
+
+
+parser = argparse.ArgumentParser(
+    prog='cat-dirs',
+    description='Concatenate source files from subdirectories into a single text file for each subdir'
+)
+
+parser.add_argument('base_directory')
+
+args = parser.parse_args()
+
+path = Path(args.base_directory)
+
+if not path.is_dir():
+    raise Exception("given path is not a directory")
+
+SOURCE_FILE_EXTENSIONS = (
+    'h',
+    'hpp',
+    'h++',
+    'hh',
+    'c',
+    'cpp',
+    'cxx',
+    'c++',
+)
+
+for dir in path.glob('*/'):
+    if not dir.is_dir():
+        continue
+
+    dst_file_name = f'{dir.name}.cpp'
+    dst_file_path = path.joinpath(dst_file_name)
+
+    with open(dst_file_path, 'w') as fout:
+        for extension in SOURCE_FILE_EXTENSIONS:
+            for src_file in dir.rglob(f'*.{extension}'):
+                with open(src_file, 'r') as fin:
+                    contents = fin.read()
+                    fout.write(f'// === {src_file.name}:\n\n')
+                    fout.write(contents)
+                    fout.write('\n\n')
---
+++ replace-spaces-in-filenames.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import argparse
+from pathlib import Path
+
+
+parser = argparse.ArgumentParser(
+    prog='replace-spaces-in-filenames',
+    description='Replace spaces in file names'
+)
+
+parser.add_argument('directory')
+
+args = parser.parse_args()
+
+path = Path(args.directory)
+
+if not path.is_dir():
+    raise Exception("given path is not a directory")
+
+for file in path.glob('*'):
+    if ' ' in file.name:
+        new_name = file.name.replace(' ', '_')
+        file.rename(file.parent.joinpath(new_name))
